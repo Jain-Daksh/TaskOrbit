9e8b6a22404f862e146fe0de4db3297ca4366b99
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-06 20:00:30 +0530

    password fixed

--- backend/seed/create-db.ts
+++ backend/seed/create-db.ts
@@ -2,7 +2,7 @@ import { Client } from 'pg';
 
 const client = new Client({
   user: 'postgres',
-  password: 'postgres',
+  password: 'password',
   host: 'localhost',
   port: 5432,
   database: 'postgres',
