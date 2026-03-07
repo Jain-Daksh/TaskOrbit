540a69358b3e6f34b93bc911c6ff0f7d464bc791
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-07 23:26:01 +0530

    cors setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,25 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
-
+import cors from 'cors';
+import router from './routes/index';
 const { Pool } = pg;
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// create postgres pool
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// prisma adapter
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
@@ -24,19 +29,7 @@ const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
 });
 
-app.get('/', async (req, res) => {
-  try {
-    const tiers = await prisma.tier.findMany();
-
-    res.json({
-      message: 'Server running 🚀',
-      tiers,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
