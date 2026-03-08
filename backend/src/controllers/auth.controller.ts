c2386b3cfecb5e89d67b08d47344d42bc582a886
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-08 10:00:56 +0530

    added safety and send access and refresh token

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -31,7 +31,11 @@ export class AuthController {
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
-      return Success(res, 'Login successful', user);
+      return Success(res, 'Login successful', {
+        user,
+        accessToken,
+        refreshToken,
+      });
     } catch (err: any) {
       return Failed(res, err.message || 'Login failed', 400, err);
     }
