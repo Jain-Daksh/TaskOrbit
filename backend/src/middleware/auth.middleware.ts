8db4a391bd669fd7480564f24676b262421db7e2
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-08 10:02:38 +0530

    changed from cookie to headers now access token in header and refresh token in cookie instead of both in cookie @ps need to rethink this weather to keep in header or cookie need to research a little bit more to find all the pros and cons

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,19 +1,33 @@
-import jwt from 'jsonwebtoken';
-import { Router, Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+interface JwtPayload {
+  userId: string;
+}
 
 export const authMiddleware = (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
-  const token = req.cookies.access_token;
+  try {
+    const authHeader = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET!
+    ) as JwtPayload;
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
-
-  const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-
-  req.user = decoded;
-  next();
 };
