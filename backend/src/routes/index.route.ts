ab1f0fcf721c0f406a0c1160b9cdf87534acedbb
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-09 21:20:53 +0530

    added task

--- backend/src/routes/index.route.ts
+++ backend/src/routes/index.route.ts
@@ -4,6 +4,7 @@ import authRouter from './auth.route';
 import workspaceRouter from './workspace.route';
 import TierRouter from './tier.route';
 import ProjectRouter from './project.route';
+import TaskRouter from './task.route';
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.use(helloRouter);
 router.use('/auth', authRouter);
 router.use('/tier', TierRouter);
 router.use('/projects', ProjectRouter);
+router.use('/task', TaskRouter);
 
 router.use('/workspaces', workspaceRouter);
 
