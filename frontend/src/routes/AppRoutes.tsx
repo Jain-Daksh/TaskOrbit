01b78caaa1660ad0f8c0618d667495944d4205c5
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-10 23:36:28 +0530

    login and regsiter url added

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import AppLayout from '../components/layout/AppLayout';
+import AppLayout from '../components/layout/dashboard/AppLayout';
 import Home from '../pages/home/Home';
+import Login from '../pages/auth/Login';
+import Register from '../pages/auth/Register';
 
 const Dashboard = () => <h1>Dashboard</h1>;
 const Projects = () => <h1>Projects</h1>;
@@ -8,6 +10,9 @@ const Tasks = () => <h1>Tasks</h1>;
 
 export const router = createBrowserRouter([
   { path: '/', element: <Home /> },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
+
   {
     path: '/app',
     element: <AppLayout />,
