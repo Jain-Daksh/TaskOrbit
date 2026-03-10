dffb1359364f259e2850df59994e3901e31a23d3
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-10 21:16:46 +0530

    sidebar done

---
+++ frontend/src/components/layout/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { Layout, Menu } from 'antd';
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  CheckSquareOutlined,
+} from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+const { Sider } = Layout;
+
+export default function Sidebar() {
+  return (
+    <Sider theme='dark'>
+      <div style={{ color: 'white', padding: 16, fontWeight: 'bold' }}>
+        Task Manager
+      </div>
+
+      <Menu
+        theme='dark'
+        mode='inline'
+        items={[
+          {
+            key: 'dashboard',
+            icon: <DashboardOutlined />,
+            label: <Link to='/'>Dashboard</Link>,
+          },
+          {
+            key: 'projects',
+            icon: <ProjectOutlined />,
+            label: <Link to='/projects'>Projects</Link>,
+          },
+          {
+            key: 'tasks',
+            icon: <CheckSquareOutlined />,
+            label: <Link to='/tasks'>Tasks</Link>,
+          },
+        ]}
+      />
+    </Sider>
+  );
+}
