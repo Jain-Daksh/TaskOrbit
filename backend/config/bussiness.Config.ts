2691ac46269571454d84849cb5f6232e471f6e80
Daksh Jain <daksh.jain@woodenstreet.com>
2026-03-13 17:13:52 +0530

    added nodemailer

--- backend/config/bussiness.Config.ts
+++ backend/config/bussiness.Config.ts
@@ -1,3 +1,11 @@
 export const config = {
   SALT_ROUNDS: 10,
+  SMTP: {
+    HOST: process.env.SMTP_HOST,
+    PORT: Number(process.env.SMTP_PORT),
+    USER: process.env.SMTP_USER,
+    PASS: process.env.SMTP_PASS,
+    FROM_NAME: 'MyApp',
+    FROM_EMAIL: 'no-reply@myapp.com',
+  },
 };
