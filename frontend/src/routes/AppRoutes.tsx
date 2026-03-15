cab75bbcd5b036c8176ee9d9aa56e670529801c8
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-03-15 17:32:40 +0530

    added protected route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -7,10 +7,9 @@ import WorkspaceDetailPage from '../pages/workspace/WorkspaceDetail';
 import WorkspacePage from '../pages/workspace/Workspace';
 import MyProfilePage from '../pages/profile/profile';
 import ProjectDetailPage from '../pages/project/projectpage';
+import ProtectedRoute from './ProtectedRoute';
 
 const Dashboard = () => <h1>Dashboard</h1>;
-const Projects = () => <h1>Projects</h1>;
-const Tasks = () => <h1>Tasks</h1>;
 
 export const router = createBrowserRouter([
   { path: '/', element: <Home /> },
@@ -19,17 +18,20 @@ export const router = createBrowserRouter([
 
   {
     path: '/app',
-    element: <AppLayout />,
+    element: <ProtectedRoute />,
     children: [
-      { index: true, element: <Dashboard /> },
-      { path: 'projects', element: <Projects /> },
-      { path: 'tasks', element: <Tasks /> },
-      { path: 'workspaces', element: <WorkspacePage /> },
-      { path: 'workspace/:id', element: <WorkspaceDetailPage /> },
-      { path: 'profile', element: <MyProfilePage /> },
       {
-        path: 'projects/:workspaceId/:projectId',
-        element: <ProjectDetailPage />,
+        element: <AppLayout />,
+        children: [
+          { index: true, element: <Dashboard /> },
+          { path: 'workspaces', element: <WorkspacePage /> },
+          { path: 'workspace/:id', element: <WorkspaceDetailPage /> },
+          { path: 'profile', element: <MyProfilePage /> },
+          {
+            path: 'projects/:workspaceId/:projectId',
+            element: <ProjectDetailPage />,
+          },
+        ],
       },
     ],
   },
